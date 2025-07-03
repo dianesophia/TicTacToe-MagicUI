29a6c046f3756e0c940b2d0dd1b45645a7796a27
Diane <dianesophia10@gmail.com>
2025-07-03 19:12:49 +0800

    Footer adjust

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="w-full text-center py-3 pt-10 text-sm text-gray-500 dark:text-gray-400 bg-transparent fixed bottom-0 left-0 z-50 pointer-events-none select-none">
+  <footer className="w-full text-center py-3 pt-16 sm:pt-10 text-sm text-gray-500 dark:text-gray-400 bg-transparent fixed bottom-0 left-0 z-50 pointer-events-none select-none">
     Built using <span className="font-semibold">Next.js</span>, <span className="font-semibold">React, Tailwind CSS</span>, and <span className="font-semibold">Magic UI</span>. All rights reserved Diane Sophia.
   </footer>
 );
